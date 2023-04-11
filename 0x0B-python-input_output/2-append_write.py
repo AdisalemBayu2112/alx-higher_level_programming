dbb733fff7c9a0b6c7b85c60cbb874d4658d42ea
AdisalemBay2112 <adisalembayu@gmail.com>
2023-04-11 13:03:23 -0700

    all mandatory tasks

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 2-append_write.py
+"""
+Defines a file-appending function.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Append a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file.
+        text (str): The text to append.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
